4f66f07b78d0408aa1f568bf111504eb94bd4a44
Einar Holsbø <einar@cs.uit.no>
2016-12-05 15:42:12 +0100

    diverse bugfix

--- lib/generate_samples.R
+++ lib/generate_samples.R
@@ -1,10 +1,11 @@
 # draw samples with 1/4 prob of "response", 2/5 prob of "stratum"
 generate_samples <- function(n, response_t=.25, stratum_t=.4, equal_variance=T) {
   ev <- ifelse(equal_variance, 0, 1)
-  response <- as.factor(rbinom(n, 1, response_t))
-  stratum <- as.factor(rbinom(n, 1, response_t))
-  stratas <- rbinom(length(lab),1,.4)   #clinical with proba .4, screening with .6
-  x <- lab + stratas  + rnorm(n, 0, 2+2*lab*ev)
+  response <- rbinom(n, 1, response_t)
+  stratum <- rbinom(n, 1, response_t)
+  x <- response + stratum  + rnorm(n, 0, 2+2*response*ev)
 
+  response <- as.factor(response)
+  stratum <- as.factor(stratum)
   tibble::tibble(response, stratum, x)
 }
