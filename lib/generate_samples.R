c463e5bb026fcd8a14d68b68182eddcfcc91d217
Einar Holsbø <einar@cs.uit.no>
2016-12-05 17:48:53 +0100

    bugfix

--- lib/generate_samples.R
+++ lib/generate_samples.R
@@ -2,8 +2,8 @@
 generate_samples <- function(n, response_t=.25, stratum_t=.4, equal_variance=T) {
   ev <- ifelse(equal_variance, 0, 1)
   response <- rbinom(n, 1, response_t)
-  stratum <- rbinom(n, 1, response_t)
-  x <- response + stratum  + rnorm(n, 0, 2+2*response*ev)
+  stratum <- rbinom(n, 1, stratum_t)
+  x <- response + stratum  + rnorm(n, 0, 2 + (2*response*ev))
 
   response <- as.factor(response)
   stratum <- as.factor(stratum)
