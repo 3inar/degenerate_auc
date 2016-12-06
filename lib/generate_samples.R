3edf410280d87c69a9b9089eed1d6a23562e2651
Einar Holsbø <einar@cs.uit.no>
2016-12-06 14:58:14 +0100

    adjust paradoxality

--- lib/generate_samples.R
+++ lib/generate_samples.R
@@ -12,14 +12,14 @@ generate_samples <- function(n, response_t=.25, stratum_t=.4, equal_variance=T) 
 
 # draw samples with 1/4 prob of "response", 2/5 prob of "stratum"
 # complete paradox: shift = 0. Shift moves {x: response==1}
-paradox_generator <- function(n, shift=0, response_t=.25, stratum_t=.4, equal_variance=T) {
+paradox_generator <- function(n, paradoxality=1, response_t=.25, stratum_t=.4, equal_variance=T) {
   ev <- ifelse(equal_variance, 0, 1)
   response <- rbinom(n, 1, response_t)
   stratum <- rbinom(n, 1, stratum_t)
 
-  ifelse(response==1, shift, 0)
-  mus <- ifelse(response == stratum, 0, 1)
+  shift <- 2*(1-paradoxality)
 
+  mus <- ifelse(response == stratum, 0, 1) + ifelse(response==1, shift, 0)
   x <- mus  + rnorm(n, 0, 2 + (2*response*ev))
 
   response <- as.factor(response)
