53f3bed3503cae409f4b0c6be140a405c7eb2f5a
Einar Holsbø <einar@cs.uit.no>
2016-12-09 13:21:49 +0100

    fix figure size

--- src/plots/paradoxality_figure.R
+++ src/plots/paradoxality_figure.R
@@ -1,7 +1,7 @@
 library(ProjectTemplate)
 load.project()
 
-dev(png, file="figures/paradoxality.png")
+png(file="figures/paradoxality.png", width=800)
 plot(paradox_results, bty="n", type="o", pch=18, col="grey25", xlab="paradoxality",
      ylab="fraction AUC < .5")
 dev.off()
