0b8d8e123382dee88633373f24b721d120f73da9
Einar Holsbø <einar@cs.uit.no>
2017-03-15 23:24:25 +0100

    investigate whether stratification fixes this problem

---
+++ src/stratified_cross_val.R
@@ -0,0 +1,35 @@
+library(ProjectTemplate)
+load.project()
+
+################################################################################
+#               Stratified cross-validation: does it help?
+################################################################################
+library(plyr)
+library(tibble)
+
+n_samples <- 440
+
+nostrat <- raply(350, function() {
+  dat <- paradox_generator(n_samples, paradoxality=1)
+  res <- raply(500, fivefold(dat))
+
+  median(na.omit(res[, 1]))
+}, .progress = "text")
+
+strat <- raply(350, function() {
+  dat <- paradox_generator(n_samples, paradoxality=1)
+  res <- raply(500, fivefold(dat, strata = dat$stratum))
+
+  median(na.omit(res[, 1]))
+}, .progress = "text")
+
+
+full_strat <- raply(350, function() {
+  dat <- paradox_generator(n_samples, paradoxality=1)
+  fullstrat <- as.factor(paste(dat$stratum, dat$response))
+  res <- raply(500, fivefold(dat, strata = fullstrat))
+
+  median(na.omit(res[, 1]))
+}, .progress = "text")
+# paradox_results <- tibble(paradox_degree, rate)
+# save(paradox_results, file="data/stratified_xval.rda")
